431f88e6f27e0ec164f6cad2165eb2ec95c4b4a5
PublicJoe <publicjoe@gmail.com>
2020-06-30 13:39:44 +0100

    Add array ES6 info

--- es6/test.js
+++ es6/test.js
@@ -1,6 +1,19 @@
-let circles = [
-    10, 30, 50
+let employees = [
+    {name: 'Dave', salary: 20000},
+    {name: 'Dozey', salary: 19500},
+    {name: 'Beaky', salary: 18000},
+    {name: 'Mick', salary: 21500},
+    {name: 'Titch', salary: 19000}
 ];
 
-let areas = circles.map(radius => Math.floor(Math.PI * radius * radius));
-console.log(areas);
+const empSalaries = employees.map((emp) => emp.salary);
+
+const totalSalary = empSalaries.reduce((sum,value)=>sum+value, 0);
+
+console.log(totalSalary);
+
+const salaryTotal = employees.reduce(function (acc, curr) {
+    return acc + curr.salary;
+}, 0);
+
+console.log(salaryTotal);
