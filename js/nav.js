93eb09bf9a94659553445020f1f211f8ec72a316
Michael Kitchen <38534817+publicjoe@users.noreply.github.com>
2025-09-08 17:16:33 +0000

    try to update links again

--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,37 @@
 $(document).ready(function() {
-    $("#paint").on("click", function() { $("#main").load("paint/index.html"); });
-    $("#2020").on("click", function() { $("#main").load("paint/2020.html"); });
-    $("#2021").on("click", function() { $("#main").load("paint/2021.html"); });
-    $("#2022").on("click", function() { $("#main").load("paint/2022.html"); });
-    $("#2023").on("click", function() { $("#main").load("paint/2023.html"); });
-    $("#2024").on("click", function() { $("#main").load("paint/2024.html"); });
-    $("#2025").on("click", function() { $("#main").load("paint/2025.html"); });
+    $("#paint").on("click", function() { 
+        $("#main").load("paint/index.html", function() {
+            window.scrollTo(0, 0);
+        });
+    });
+    $("#2020").on("click", function() { 
+        $("#main").load("paint/2020.html", function() {
+            window.scrollTo(0, 0);
+        });
+    });
+    $("#2021").on("click", function() { 
+        $("#main").load("paint/2021.html", function() {
+            window.scrollTo(0, 0);
+        });
+    });
+    $("#2022").on("click", function() { 
+        $("#main").load("paint/2022.html", function() {
+            window.scrollTo(0, 0);
+        });
+    });
+    $("#2023").on("click", function() { 
+        $("#main").load("paint/2023.html", function() {
+            window.scrollTo(0, 0);
+        });
+    });
+    $("#2024").on("click", function() { 
+        $("#main").load("paint/2024.html", function() {
+            window.scrollTo(0, 0);
+        });
+    });
+    $("#2025").on("click", function() { 
+        $("#main").load("paint/2025.html", function() {
+            window.scrollTo(0, 0);
+        });
+    });
 });
-
-/*
-<script>
-    $(document).ready( function() { $("#sm").on("click", function() { $("#main").load("40k/index.html"); }); });
-    $(document).ready( function() { $("#paint").on("click", function() { $("#main").load("paint/index.html"); }); });
-    $(document).ready( function() { $("#2020").on("click", function() { $("#main").load("paint/2020.html"); }); });
-    $(document).ready( function() { $("#2021").on("click", function() { $("#main").load("paint/2021.html"); }); });
-    $(document).ready( function() { $("#2022").on("click", function() { $("#main").load("paint/2022.html"); }); });
-    $(document).ready( function() { $("#2023").on("click", function() { $("#main").load("paint/2023.html"); }); });
-    $(document).ready( function() { $("#2024").on("click", function() { $("#main").load("paint/2024.html"); }); });
-    $(document).ready( function() { $("#2025").on("click", function() { $("#main").load("paint/2025.html"); }); });
-</script>
-*/
