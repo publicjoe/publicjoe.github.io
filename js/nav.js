d7c64d18fd2cc467f5256ad2baf9a1a770ca9fb1
Michael Kitchen <38534817+publicjoe@users.noreply.github.com>
2025-09-08 17:09:17 +0000

    test nav change

---
+++ js/nav.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $("#paint").on("click", function() { $("#main").load("paint/index.html"); });
+    $("#2020").on("click", function() { $("#main").load("paint/2020.html"); });
+    $("#2021").on("click", function() { $("#main").load("paint/2021.html"); });
+    $("#2022").on("click", function() { $("#main").load("paint/2022.html"); });
+    $("#2023").on("click", function() { $("#main").load("paint/2023.html"); });
+    $("#2024").on("click", function() { $("#main").load("paint/2024.html"); });
+    $("#2025").on("click", function() { $("#main").load("paint/2025.html"); });
+});
+
+/*
+<script>
+    $(document).ready( function() { $("#sm").on("click", function() { $("#main").load("40k/index.html"); }); });
+    $(document).ready( function() { $("#paint").on("click", function() { $("#main").load("paint/index.html"); }); });
+    $(document).ready( function() { $("#2020").on("click", function() { $("#main").load("paint/2020.html"); }); });
+    $(document).ready( function() { $("#2021").on("click", function() { $("#main").load("paint/2021.html"); }); });
+    $(document).ready( function() { $("#2022").on("click", function() { $("#main").load("paint/2022.html"); }); });
+    $(document).ready( function() { $("#2023").on("click", function() { $("#main").load("paint/2023.html"); }); });
+    $(document).ready( function() { $("#2024").on("click", function() { $("#main").load("paint/2024.html"); }); });
+    $(document).ready( function() { $("#2025").on("click", function() { $("#main").load("paint/2025.html"); }); });
+</script>
+*/
