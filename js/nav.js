ea30e582150d20f584a70289b83a61b27536e470
Michael Kitchen <38534817+publicjoe@users.noreply.github.com>
2025-10-02 14:54:07 +0000

    software added

--- js/nav.js
+++ js/nav.js
@@ -26,39 +26,9 @@ $(document).ready(function() {
         loadAndScroll("paint/2025.html");
     });
     $("#aws").on("click", function() { 
+        loadAndScroll("software/software.html");
+    });
+    $("#aws").on("click", function() { 
         loadAndScroll("software/aws.html");
-    });
-    $("#azure").on("click", function() { 
-        loadAndScroll("software/azure.html");
-    });
-    $("#csharp").on("click", function() { 
-        loadAndScroll("software/csharp.html");
-    });
-    $("#docker").on("click", function() { 
-        loadAndScroll("software/docker.html");
-    });
-    $("#git").on("click", function() { 
-        loadAndScroll("software/git.html");
-    });
-    $("#javascript").on("click", function() { 
-        loadAndScroll("software/javascript.html");
-    });
-    $("#mysql").on("click", function() { 
-        loadAndScroll("software/sql.html");
-    });
-    $("#python").on("click", function() { 
-        loadAndScroll("software/python.html");
-    });
-    $("#react").on("click", function() { 
-        loadAndScroll("software/react.html");
-    });
-    $("#typescript").on("click", function() { 
-        loadAndScroll("software/typescript.html");
-    });
-    $("#unity").on("click", function() { 
-        loadAndScroll("software/unity.html");
-    });
-    $("#ai").on("click", function() { 
-        loadAndScroll("software/ai.html");
     });
 });
